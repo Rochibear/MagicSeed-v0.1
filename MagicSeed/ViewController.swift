ff6cd3aca1b1dc27d5804a113af51f1a463ebf65
Rochibear <killpmite@gmail.com>
2024-11-19 23:40:57 +0800

    Initial Commit

---
+++ MagicSeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicSeed
+//
+//  Created by CihYang Su on 2024/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
